f2719b6a029304a89e439af50df62bf0ec042558
Uítalo <uitalo.urss@gmail.com>
2022-02-20 20:24:39 -0300

    colocando a ação de concluir e task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(!newTaskTitle) return;
 
     const newTask = {
@@ -32,6 +31,12 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const completedTask = tasks.map(task =>
+      task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+      } : task)
+    setTasks(completedTask);
   }
 
   function handleRemoveTask(id: number) {
